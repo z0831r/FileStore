155feb27833075d509286756054b70b37df8d8a7
z0831r <122157816+z0831r@users.noreply.github.com>
2026-01-14 10:24:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -42,16 +42,16 @@ PROTECT = False
 # Messages Configuration
 MESSAGES = {
     "START": "<b>👋 你好!!, {first} ~ <blockquote>我是您的私人文件管家。\n直接发送文件给我，我会为您生成永久分享链接。\n\n⚠️ 注意：请勿上传违规内容。</blockquote></b>",
-    "FSUB": "<b><blockquote>🚨 需关注频道 ×</blockquote>\n  您的文件已准备就绪 ‼️\n\n检测到您尚未关注我们的频道，为了防止滥用，请点击下方按钮关注，然后点击“刷新重试”即可获取文件。</b>",
+    "FSUB": "<b>🚨 需关注频道 🚨\n\n检测到您尚未关注我们的频道。\n为了防止滥用，请点击下方按钮关注，然后点击“刷新重试”获取文件。</b>",
     "ABOUT": "<b>›› 机器人名称: 文件存储机器人 \n <blockquote expandable>›› 更新频道: <a href='https://t.me/codeflix_bots'>点击查看</a> \n›› 主人: @ProYato\n›› 语言: <a href='https://docs.python.org/3/'>Python 3</a> \n›› 框架: <a href='https://docs.pyrogram.org/'>Pyrogram v2</a> \n›› 数据库: <a href='https://www.mongodb.com/docs/'>Mongo DB</a></b></blockquote>",
     "REPLY": "<b>收到！正在为您生成链接，请稍候...</b>",
     "SHORT_MSG": "<b>📊 嘿 {first}, \n\n‼️ 获取您的文件链接 ‼️\n\n ⌯ 链接已生成，请点击下方按钮打开..</b>",
-    "START_PHOTO": "https://github.com/z0831r/FileStore/blob/Yato/photo_2026-01-13_19-08-13.jpg?raw=true",
-    "FSUB_PHOTO": "🚨 需关注频道 🚨\n\n检测到您尚未关注我们的频道。\n为了防止滥用，请点击下方按钮关注，然后点击“刷新重试”获取文件。",
-    "SHORT_PIC": "https://github.com/z0831r/FileStore/blob/Yato/photo_2026-01-13_19-08-13.jpg?raw=true",
-    "SHORT": "https://github.com/z0831r/FileStore/blob/Yato/photo_2026-01-13_19-08-13.jpg?raw=true"
+    # ⚠️ 必须填入有效链接！这里使用了一张绝对能用的测试图，先让机器人活过来
+    "START_PHOTO": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
+    "FSUB_PHOTO": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
+    "SHORT_PIC": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
+    "SHORT": "https://telegra.ph/file/8aaf4df8c138c6685dcee-05d3b183d4978ec347.jpg"
 }
-
 def LOGGER(name: str, client_name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     formatter = logging.Formatter(
