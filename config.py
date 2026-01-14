505aed61c7a7a660c539c8da097e15ee6bcbf709
z0831r <122157816+z0831r@users.noreply.github.com>
2026-01-14 09:37:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
 
 MSG_EFFECT = 5046509860389126442
 
-SHORT_URL = "linkshortify.com" # shortner url 
-SHORT_API = "" 
-SHORT_TUT = "https://t.me/How_to_Download_7x/26"
+SHORT_URL = ""
+SHORT_API = ""
+SHORT_TUT = ""
 
 # Bot Configuration
 SESSION = "yato"
@@ -46,10 +46,10 @@ MESSAGES = {
     "ABOUT": "<b>›› 机器人名称: 文件存储机器人 \n <blockquote expandable>›› 更新频道: <a href='https://t.me/codeflix_bots'>点击查看</a> \n›› 主人: @ProYato\n›› 语言: <a href='https://docs.python.org/3/'>Python 3</a> \n›› 框架: <a href='https://docs.pyrogram.org/'>Pyrogram v2</a> \n›› 数据库: <a href='https://www.mongodb.com/docs/'>Mongo DB</a></b></blockquote>",
     "REPLY": "<b>收到！正在为您生成链接，请稍候...</b>",
     "SHORT_MSG": "<b>📊 嘿 {first}, \n\n‼️ 获取您的文件链接 ‼️\n\n ⌯ 链接已生成，请点击下方按钮打开..</b>",
-    "START_PHOTO": "[url=https://ibb.co/GQpshWjr][img]https://i.ibb.co/YBQDrcGV/photo-2026-01-13-19-08-13.jpg[/img][/url]",
-    "FSUB_PHOTO": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
-    "SHORT_PIC": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
-    "SHORT": "https://telegra.ph/file/8aaf4df8c138c6685dcee-05d3b183d4978ec347.jpg"
+    "START_PHOTO": "https://github.com/z0831r/FileStore/blob/Yato/photo_2026-01-13_19-08-13.jpg?raw=true",
+    "FSUB_PHOTO": "https://github.com/z0831r/FileStore/blob/Yato/photo_2026-01-13_19-08-13.jpg?raw=true",
+    "SHORT_PIC": "https://github.com/z0831r/FileStore/blob/Yato/photo_2026-01-13_19-08-13.jpg?raw=true",
+    "SHORT": "https://github.com/z0831r/FileStore/blob/Yato/photo_2026-01-13_19-08-13.jpg?raw=true"
 }
 
 def LOGGER(name: str, client_name: str) -> logging.Logger:
